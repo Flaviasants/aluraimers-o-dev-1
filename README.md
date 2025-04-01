c007233c23e8b28fad5784bb947035ea5f363c6a
Flaviasants <flaviasantoss.contato@gmail.com>
2025-04-01 00:29:36 -0300

    foi desenvolvido um conversor de moedas

---
+++ README.md
@@ -0,0 +1,6 @@
+# Alura - Imersão Dev 1 
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/Flaviasants/pen/MYWzzzr](https://codepen.io/Flaviasants/pen/MYWzzzr).
+
---
+++ dist/script.js
@@ -0,0 +1,3 @@
+valorgaleão = prompt("Quantos Galeões deseja trocar nos cofres de Gringotes?")
+umgaleão = 0.0040
+alert("💰 Clink! Sua conversão mágica está pronta: R$" + valorgaleão * umgaleão)
